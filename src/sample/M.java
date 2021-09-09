afdb8df38fb37c416a27817a95e0677d87deb9c3
premkumar15 <premkumar.datti@gmail.com>
2021-09-09 18:03:50 +0530

    message

---
+++ src/sample/M.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class M {
+		public void sysout() {
+			int q=123;
+			int a=12;
+			int b= q+a;
+
+	}
+	public static void main(String[] args) {
+		M q = new M();
+		q.sysout();
+
+	}
+
+}
